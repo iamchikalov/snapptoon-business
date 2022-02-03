fc15fe3d1440ab43d09d33664e6ffe850a5f9a7e
S1ngle322 <danya.sinelnik@mail.ru>
2022-02-03 12:30:07 +0300

    feat: change-pass-and-email-api

--- src/types/dtos/register.dto.ts
+++ src/types/dtos/register.dto.ts
@@ -3,15 +3,24 @@ import { Asset } from '@snapptoon/backend-common/src/data/models/Asset'
 import {UserRole} from '../enums';
 import { SocialLinks } from '@snapptoon/backend-common/src/data/models/SocialLinks'
 import { Inbox } from '@snapptoon/backend-common/src/data/models/Inbox'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class RegisterDto extends BaseDto {
+    @ApiProperty()
     email: string
+    @ApiProperty()
     password: string
+    @ApiProperty()
     name: string
+    @ApiProperty()
     asset?: Asset[]
     type?: UserRole
+    @ApiProperty()
     description?: string
+    @ApiProperty()
     isVerified?: boolean
+    @ApiProperty()
     links?: SocialLinks
+    @ApiProperty()
     notifications?: Inbox[]
 }
