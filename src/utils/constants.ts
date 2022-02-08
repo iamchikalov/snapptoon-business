1c26e317a8dc17410f44e70287bfcd11a5161b14
pussycoder <egorforlovecom@gmail.com>
2022-02-09 02:03:56 +0300

    normal auth key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const SALT_ROUNDS = 10
-export const AUTH_SECRET_KEY = 'suppaPuppaUltraSecret'
+export const AUTH_SECRET_KEY = '9246aea47d29178947be421a579fecf8863521fd19571b676f6b53f747364cf568a4d5cfeb9732055bcab8c0cea5101282a1e95c48881d88be621a1a52f4b620'
