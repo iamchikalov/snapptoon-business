e263d7b014b6c4e5d6799472f636e6a689f64088
S1ngle322 <danya.sinelnik@mail.ru>
2022-02-12 17:12:59 +0300

    debug shreks to server

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -11,7 +11,7 @@ export class RegisterController {
 
     @Post('/api/register')
     async register(@Body() registerDTO: RegisterDto, @Request() request) {
-      const verificationLink = `${request.protocol}://${request.hostname}/verify-account/`
+      const verificationLink = `${request.protocol}://${request.headers.hostname}/verify-account/`
       console.log(verificationLink)
       return await this.service.createAccount(registerDTO, verificationLink)
     }
