154885370a590b5f2dc9de464f7743558f69411c
S1ngle322 <danya.sinelnik@mail.ru>
2022-02-15 19:29:46 +0300

    hotfix: logo storing

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { UserService } from '../services'
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common'
+import { Body, Controller, Get, Param, Patch, UploadedFiles, UseInterceptors } from '@nestjs/common'
 import { UserDto } from '../types/dtos'
+import { FileFieldsInterceptor } from '@nestjs/platform-express'
+import { ApiBody, ApiConsumes } from '@nestjs/swagger'
 
 @Controller()
 export class UserController {
@@ -24,7 +26,32 @@ export class UserController {
   }
 
   @Patch('/api/change-user-data')
-  async changeUserData (@Body() userDto: UserDto) {
-    return await this.service.changeUserData(userDto)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        access_token: {
+          type: 'string'
+        },
+        name: {
+          type: 'string',
+        },
+        description: {
+          type: 'string',
+        },
+        logo: {
+          type: 'file',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileFieldsInterceptor([{ name: 'logo', maxCount: 1 }]))
+  async changeUserData (
+    @UploadedFiles() file: { logo: Express.Multer.File[] },
+    @Body() userDto: UserDto
+  ) {
+    return await this.service.changeUserData(file, userDto)
   }
 }
