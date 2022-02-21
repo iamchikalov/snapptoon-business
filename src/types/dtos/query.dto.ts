36740a6b311dba3264933a174b50171fbd37b955
S1ngle322 <danya.sinelnik@mail.ru>
2022-02-21 10:10:18 +0300

    fix: Properties for api/v1/assets+resolved conflicts

--- src/types/dtos/query.dto.ts
+++ src/types/dtos/query.dto.ts
@@ -1,8 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
 
 export class QueryDto {
+  @ApiProperty()
   author: string
+  @ApiProperty()
   category?: string
+  @ApiProperty()
   page?: number
+  @ApiProperty()
   perPage?: number
+  @ApiProperty()
   sort?: string
 }
